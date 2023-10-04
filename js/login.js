0999e3dbd1906cc47a07c31a1ff4f2c6b1cadc8d
Mazhit <kajsar02@gmail.com>
2023-10-04 23:26:47 +0300

    login-page

---
+++ js/login.js
@@ -0,0 +1,16 @@
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+const continueBtn = document.getElementById("continue");
+
+const Login = "qwerty";
+const Password = 123;
+
+function isValid(){
+    if (login === Login || password === Password){
+        continueBtn.addEventListener('click', () => {
+            console.log('assdfsghs')
+            window.location.href = 'adminPanel.html'
+        })
+    }
+}
+isValid()
